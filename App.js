3bc76dca80e77f3cd6b1a8f1c828de2e4a57d630
Hassad674 <h.smara@it-students.fr>
2024-01-30 16:46:55 +0100

    step3

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { Platform, Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import * as Location from "expo-location";
 
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -17,14 +17,29 @@ export default function App() {
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+      fetchWeather(location.coords.latitude, location.coords.longitude);
     })();
   }, []);
+
+  const fetchWeather = (lat, lon) => {
+    const apiKey = "20ef76bb24da80a53872ce8404193ebe";
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setWeather(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   let text = "Waiting..";
   if (errorMsg) {
     text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
+  } else if (weather) {
+    text = `Weather in ${weather.name}: ${weather.main.temp} °C, ${weather.weather[0].description}`;
   }
 
   return (
